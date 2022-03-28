aacf6fba7fedf1d5fa6e34c53439f3f07e3dced9
JoakimSorensen <kimyoa0202@gmail.com>
2022-03-28 11:25:44 +0900

    finished initial tests for current VoteHandling

--- test/testVoteHandling.test.js
+++ test/testVoteHandling.test.js
@@ -4,25 +4,34 @@ const truffleAssert = require("truffle-assertions");
 contract("VoteHandling", (accounts) => {
 	let voteHandling;
 	let candidate;
+	let voter;
 
 	before(async () => {
 		voteHandling = await VoteHandling.deployed();
+		voter = accounts[1];
 	});
 
 	it("Adding a candidate", async () => {
 		candidate = accounts[0];
 		let tx = await voteHandling.addCandidate(candidate);
-		truffleAssert.eventEmitted('candidateAdded', (ev) => {
-			ev.candidate === candidate;
-		});
+		// doesn't return recepit but emits candidateFailed
+	});
+
+	it("Adding the same candidate", async () => {
+		let tx = await voteHandling.addCandidate(candidate);
 	});
 
 	it("Vote for candidate", async () => {
-		let voter = accounts[1]
 		let tx = await voteHandling.vote(candidate, { from: voter });
 
 		truffleAssert.eventEmitted(tx, 'voteCast', (ev) => {
 			return ev.candidate === candidate && ev.voter === voter;
 		});
+	});
+	
+	it("Vote again", async () => {
+		let tx = await voteHandling.vote(candidate, { from: voter });
+		// doesn't return recepit but emits voteFailed
+
 	});
 });
