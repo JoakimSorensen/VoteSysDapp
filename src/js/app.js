d0e5b70138f41aa7038dd5a2746681c51deed0d9
JoakimSorensen <kimyoa0202@gmail.com>
2022-04-13 12:15:03 +0900

    removed non-async

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,18 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
+  init: async function () {
     return await App.initWeb3();
   },
+	
 
-  initWeb3: async function() {
+  initWeb3: async function () {
 	// Modern dapp browsers
 	var Web3 = require("web3");
+
 	if (window.ethereum) {
 		App.web3Provider = window.ethereum;
+		
 		try {
 			// request account access
 			await window.ethereum.request({ method: "eth_requestAccounts"});
@@ -18,21 +21,25 @@ App = {
 			console.error("User denied access");
 			console.log(error);
 		}
+
 	} else if (window.web3) {
-	// Legacy dapp browsers
-	App.web3Provider = window.web3.currentProvider;
+	
+		// Legacy dapp browsers
+		App.web3Provider = window.web3.currentProvider;
+	
 	} else {
-	// if no injected web3 instance is detected, fall back to develop network
-		App.web3Provider = new Web3.providers.HttpProvider("http://localhost:8545");
+		// if no injected web3 instance is detected, 
+		// fall back to develop network
+		App.web3Provider = new Web3.providers
+								   .HttpProvider("http://localhost:8545");
 	}
 
-	
-
 	web3 = new Web3(App.web3Provider);
-    return App.initContractAs();
+    return App.initContract();
   },
 
-  initContractAs: async function () {
+
+  initContract: async function () {
 
 	await $.getJSON('VoteHandling.json', function (data) {
 		// Get the necessary contract artifact file and instantiate it 
@@ -47,25 +54,14 @@ App = {
 	return App.bindEvents();
   },
 
-  initContract: function() {
-	$.getJSON('VoteHandling.json', function (data) {
-		// Get the necessary contract artifact file and instantiate it 
-		// with @truffle/contract
-		var VoteHandlingArtifact = data;
-		App.contracts.VoteHandling = TruffleContract(VoteHandlingArtifact);
 
-		// set provider for contract
-		App.contracts.VoteHandling.setProvider(App.web3Provider);
-	});
-    return App.bindEvents();
+  bindEvents: function () {
+    $(document).on('click', '.btn-vote', App.handleVote);
+    $(document).on('click', '.btn-add-candidate', App.addCandidate);
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-vote', App.handleVoteAs);
-    $(document).on('click', '.btn-add-candidate', App.addCandidateAs);
-  },
 
-  addCandidateAs: async function () {
+  addCandidate: async function () {
 
 	/* For easy testing, this functionality is
 	   supposed to be on admin page or similar */
@@ -108,92 +104,27 @@ App = {
 			candidateList.push(candidates[i].eth_address);
 		}
 
-		var instance = await App.contracts.VoteHandling.deployed();
+		voteHandlingInstance = await App.contracts.VoteHandling.deployed();
 			
-		voteHandlingInstance = instance;
 		console.log('adding candidate list ' + candidateList);
 
 		await voteHandlingInstance.addCandidateList(candidateList,
 													{from: account});	
 		
-		try {	
-			console.log("Before returning getContrCand");
-			return App.getContractCandidatesAs();
-		} catch(error) {
-			console.log(error);
-		}
+		console.log("Before returning getContrCand");
+		return App.getContractCandidates();
 	});
-	
   }, 
+  
 
-  addCandidate: function () {
-	
-	/* For easy testing, this functionality is
-	   supposed to be on admin page or similar */
-	
-	var candidates;
-	var candidateList = [];
-		
-   	$.getJSON('../candidate.json', function (data) {
-	
-	  candidates = data;		
-      var candidateRow = $('#candidateRow');
-      var candidateTemplate = $('#candidateTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        candidateTemplate.find('.panel-title').text(data[i].name);
-        candidateTemplate.find('img').attr('src', data[i].picture);
-        candidateTemplate.find('.age').text(data[i].age);
-        candidateTemplate.find('.location').text(data[i].location);
-		candidateTemplate.find('.btn-vote').attr('data-id', 
-												data[i].eth_address);
-
-        candidateRow.append(candidateTemplate.html());
-      }
-		
-
-	  $(document).find('.btn-add-candidate').attr('style', 'visibility:hidden')
-											.attr('disable', true);
-	}).then(function () {
-
-		console.log(web3.eth.accounts);
-		web3.eth.getAccounts(function (error, accounts) {
-			if (error) {
-				console.log(error);
-			}
-
-			var account = accounts[0];// only returns current selected
-
-			for (i = 0; i < candidates.length; i++) {
-				console.log('Adding candidate ' + candidates[i].eth_address);
-				candidateList.push(candidates[i].eth_address);
-			}
-
-			App.contracts.VoteHandling.deployed().then(function (instance) {
-				voteHandlingInstance = instance;
-				console.log('adding candidate list ' + candidateList);
-
-				return voteHandlingInstance.addCandidateList(candidateList,
-														{from: account});	
-			}).then(function(result) {
-				console.log("Before returning getContrCand");
-				return App.getContractCandidates();
-			}).catch(function(err) {
-				console.log(err);
-			});
-		});
-	});
-  },
-
-  getContractCandidatesAs: async function () {
+  getContractCandidates: async function () {
 	var voteHandlingInstance;
     var candidateRow = $('#candidateRow');
     var candidateTemplate = $('#candidateTemplate');
 
 	console.log("in getContractCandidates");
-	var instance = await App.contracts.VoteHandling.deployed();
+	voteHandlingInstance = await App.contracts.VoteHandling.deployed();
 		
-	voteHandlingInstance = instance;
 	var candidates = await voteHandlingInstance.getCandidates.call();
 
 	if (candidates.length == 0) {
@@ -207,43 +138,12 @@ App = {
     // $('.panel-candidate').eq(i)
 	// .find('button').attr('data-id', candidates[i]);
 
-	  try {
-		return App.checkVotedAs();
-	  } catch(error) {
-		console.log(err.message);
-	  }
+	return App.checkVoted();
   },
 
-
-  getContractCandidates: function () {
-	  var voteHandlingInstance;
-      var candidateRow = $('#candidateRow');
-      var candidateTemplate = $('#candidateTemplate');
-
-	  console.log("in getContractCandidates");
-	  App.contracts.VoteHandling.deployed().then(function(instance) {
-		voteHandlingInstance = instance;
-		return voteHandlingInstance.getCandidates.call();
-	  }).then(function(candidates) {
-		if (candidates.length == 0) {
-			console.log("Couldn't retrieve candidates");
-		} 
-		console.log("candidates retrieved from contract: " + candidates);
-	
-		// If the addresses aren't in the candidates.json then bind them here.
-		// Example below:
-       		// $('.panel-candidate').eq(i)
-			// .find('button').attr('data-id', candidates[i]);
-
-	  }).then(function () {
-		return App.checkVoted();
-	  }).catch(function(err) {
-		console.log(err.message);
-	  });
-  },
 
   // check if current account has voted
-  checkVotedAs: async function () {
+  checkVoted: async function () {
 	var voteHandlingInstance;
 
 	await web3.eth.getAccounts(async (error, accounts) => {
@@ -262,43 +162,14 @@ App = {
 			$('.panel-candidate').find('button')
 								 .text('Collecting votes...')
 								 .attr('disabled', true);
-			try {
-				return App.showVotesAs();
-			} catch (error) {
-				console.log(error);
-			}
+			
+			return App.showVotes();
 		}
 	});
   },
 
-  // check if current account has voted
-  checkVoted: function() {
-	  var voteHandlingInstance;
 
-	  web3.eth.getAccounts(function(error, accounts) {
-		if (error) {
-			console.log(error);
-		}
-		var account = accounts[0];
-	  	App.contracts.VoteHandling.deployed().then(function(instance) {
-			voteHandlingInstance = instance;
-			return voteHandlingInstance.hasVoted.call(accounts);
-	  	}).then(function(hasVoted) {
-			console.log('Account ' + account + ' has voted: ' + hasVoted);
-			if (hasVoted) {
-				// disable button
-				$('.panel-candidate').find('button')
-									 .text('Collecting votes...')
-									 .attr('disabled', true);
-				return App.showVotes();
-			}
-	  	}).catch(function(err) {
-			console.log(err);
-	  	});
-	  });
-  },
-
-  showVotesAs: async function() {
+  showVotes: async function() {
 	var voteHandlingInstance;
 	var candidates;
 
@@ -319,33 +190,8 @@ App = {
 	}
   },
 
-  showVotes: function() {
-	var voteHandlingInstance;
-	var candidates;
 
-   	$.getJSON('../candidate.json', function (data) {
-		candidates = data;		
-	}).then(function() {
-
-		App.contracts.VoteHandling.deployed().then(function(instance) {
-			voteHandlingInstance = instance;
-			return voteHandlingInstance.getVotes.call();
-		}).then(function(_votes) {
-			var votes = _votes;
-			console.log('votes: ' + votes);
-			console.log('candates.length = ' + candidates.length);
-			for (i = 0; i < candidates.length; i++) {
-
-        		$('.panel-candidate').eq(i).find('button')
-										   .text('Votes: ' + votes[i]);
-			}
-		}).catch(function(err) {
-			console.log(err);
-		});
-	});
-  },
-
-  handleVoteAs: async function () {
+  handleVote: async function () {
     event.preventDefault();
 
     var candidateId = $(event.target).data('id');
@@ -366,35 +212,6 @@ App = {
 		return App.checkVoted();
 
 	});
-  },
-
-  // call the vote function
-  handleVote: function(event) {
-    event.preventDefault();
-
-    var candidateId = $(event.target).data('id');
-	var voteHandlingInstance;
-
-	web3.eth.getAccounts(function(error, accounts) {
-		if (error) {
-			console.log(error);
-		}
-
-		var account = accounts[0];
-
-		App.contracts.VoteHandling.deployed().then(function(instance) {
-			voteHandlingInstance = instance;
-			console.log('Voting for candidate: ' + candidateId);
-			
-			return voteHandlingInstance.vote(candidateId, {from: account});
-		}).then(function() {
-			return App.checkVoted();
-		}).catch(function(err) {
-			console.log(err.message);
-		});
-
-	});
-
   }
 
 };
