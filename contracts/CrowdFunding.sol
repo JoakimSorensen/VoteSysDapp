866931d1161e46decb27ee81c4ffe713bef9b245
JoakimSorensen <kimyoa0202@gmail.com>
2022-04-19 15:00:13 +0900

    started to add crowdfunding contract

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract CrowdFunding {
+	uint numberOfInvestors = 10;
+	address[] investors;
+	mapping(address => uint256) investments;
+	mapping(address => bool) isInvestor;
+
+	constructor() {
+		investors = new address[](numberOfInvestors);
+	}
+
+	function invest(uint256 amount) payable public {
+		require(msg.value == amount);
+		if (!isInvestor[msg.sender]) {
+			investors.push(msg.sender);
+			investments[msg.sender] = amount;
+			isInvestor[msg.sender] = true;
+		} else {
+			investments[msg.sender] += amount;
+		}
+	}
+
+	function getCurrentFunds() public view returns (uint256) {
+		return address(this).balance;
+	}
+}
